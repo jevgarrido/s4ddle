6e65a46b5d4ac1db95cb332f5d5918da49eee06c
José Garrido <jose.valeriogarrido@dlr.de>
2024-02-10 13:23:02 +0100

    Configure conditional compilation for std and no_std modules.

---
+++ src/embed.rs
@@ -0,0 +1,20 @@
+// MINRES
+//
+pub fn minres() {}
+
+// SYMMLQ
+//
+pub fn symmlq() {}
+
+// GCR
+//
+pub fn gcr() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_minres() {
+        let g = vec![1.0, 3.0, 5.0];
+        println!("{:?}", g);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
-
+#![cfg_attr(all(not(test), not(feature = "std")), no_std)]
+
+pub mod embed;
+
+#[cfg(feature = "std")]
+pub mod os;
